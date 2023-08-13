710629d2924053441673a79232f8410b4535e89d
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-08-13 19:38:10 +0300

    fix: move speed

--- Assets/Scenes/Scripts/Characters/Controllers/CharacterController.cs
+++ Assets/Scenes/Scripts/Characters/Controllers/CharacterController.cs
@@ -2,6 +2,7 @@ using LegoBattaleRoyal.Characters.Interfaces;
 using LegoBattaleRoyal.Characters.Models;
 using LegoBattaleRoyal.Characters.View;
 using LegoBattaleRoyal.ScriptableObjects;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace LegoBattaleRoyal.Characters.Controllers
--- Assets/Scenes/Scripts/Characters/View/CharacterView.cs
+++ Assets/Scenes/Scripts/Characters/View/CharacterView.cs
@@ -10,10 +10,8 @@ namespace LegoBattaleRoyal.Characters.View
     {
         [SerializeField] private CharacterSO _characterSO;
         private Rigidbody _rigidbody;
+        private float _moveSpeed;
 
-        private bool _isMoving = false;
-        private Vector3 _targetPosition;
-        public float _speed = 1f;
         private float moveDuration;
         private float jumpHeight;
         private Vector3 point;
@@ -22,6 +20,7 @@ namespace LegoBattaleRoyal.Characters.View
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            _moveSpeed = _characterSO.Speed;
         }
 
         private void Update()
@@ -33,79 +32,9 @@ namespace LegoBattaleRoyal.Characters.View
         {
             if (_move != null && _move.IsActive())
                 _move.Kill();
-            var moveSpeed = _characterSO.Speed;
 
-            var movePoint = new Vector3(point.x, _rigidbody.position.y, point.z);
-            _move = _rigidbody.DOJump(point, jumpHeight, 1, moveDuration);
-
-            //var positionX = moveDuration;
-            //var positionY = jumpHeight;
-
-            //var moveSpeed = _characterSO.Speed;
-
-            //var mousePosition = Input.mousePosition;
-
-            //_rigidbody.velocity = new Vector3(mousePosition.x * moveSpeed, mousePosition.y * positionY, _rigidbody.position.z);
-
-            //if (Input.GetMouseButton(0))
-            //{
-            //    _targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //    _isMoving = true;
-            //}
-
-            //if (_isMoving)
-            //{
-            //    transform.position = Vector3.MoveTowards(transform.position, _targetPosition, Time.fixedDeltaTime * _speed);
-            //    if (transform.position == _targetPosition)
-            //    {
-            //        _targetPosition = false;
-            //    }
-            //}
-
-            //if (Input.GetMouseButtonDown(0))
-            //{
-            //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            //    RaycastHit placeInfo;
-            //    if (Physics.Raycast(ray, out placeInfo))
-            //    {
-            //        if (placeInfo.collider.CompareTag("Ground"))
-            //        {
-            //            _targetPosition = new Vector3(placeInfo.point.x, transform.position.y, placeInfo.point.z);
-            //            _isMoving = true;
-            //        }
-            //    }
-            //}
-
-            //if (_isMoving == true)
-            //{
-            //    transform.LookAt(_targetPosition);
-            //    transform.Translate(new Vector3(Input.mousePosition.x /** _speed * Time.deltaTime*/, jumpHeight));
-            //    if (Vector3.Distance(_targetPosition, transform.position) < 0.01)
-            //    {
-            //        _isMoving = false;
-            //    }
-            //}
+            var movePoint = new Vector3(point.x, _rigidbody.position.y, point.z).normalized;
+            _move = _rigidbody.DOJump(movePoint * _moveSpeed, jumpHeight, 1, moveDuration);
         }
-
-        //private Vector3 FindWhereClicked(MouseState ms)
-        //{
-        //    Vector3 nearScreenPoint = new Vector3(ms.X, ms.Y, 0);
-        //    Vector3 farScreenPoint = new Vector3(ms.X, ms.Y, 1);
-
-        //    Vector3 nearWorldPoint = device.Viewport
-        //    .Unproject(nearScreenPoint, cam.projectionMatrix, cam.viewMatrix, Matrix.Identity);
-
-        //    Vector3 farWorldPoint = device.Viewport
-        //    .Unproject(farScreenPoint, cam.projectionMatrix, cam.viewMatrix, Matrix.Identity);
-
-        //    Vector3 direction = farWorldPoint - nearWorldPoint;
-
-        //    float zFactor = -nearWorldPoint.Y / direction.Y;
-
-        //    Vector3 zeroWorldPoint = nearWorldPoint + direction * zFactor;
-
-        //    return zeroWorldPoint;
-        //}
     }
 }
