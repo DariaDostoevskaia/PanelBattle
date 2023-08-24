760b2f19d5e29e3cc98e8d1263aebc6d16cc9ce9
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-08-25 00:10:57 +0300

    fix: renderer

--- Assets/Scenes/Scripts/Panels/Controllers/PanelController.cs
+++ Assets/Scenes/Scripts/Panels/Controllers/PanelController.cs
@@ -1,8 +1,10 @@
 using LegoBattaleRoyal.Panels.Models;
 using LegoBattaleRoyal.Panels.View;
 using System;
+using System.Drawing;
 using System.Linq;
 using UnityEngine;
+using Color = UnityEngine.Color;
 
 namespace LegoBattaleRoyal.Panels.Controllers
 {
@@ -16,6 +18,7 @@ namespace LegoBattaleRoyal.Panels.Controllers
 
         private (PanelModel panelModel, PanelView panelView)[] _pairs;
         private MeshRenderer _renderer;
+        private MeshRenderer _startRenderer;
 
         public PanelController((PanelModel panelModel, PanelView panelView)[] pairs)
         {
@@ -47,34 +50,35 @@ namespace LegoBattaleRoyal.Panels.Controllers
             var panelView = _pairs.First(pair => pair.panelView == view).panelView;
 
             _renderer = panelView.GetComponent<MeshRenderer>();
+            _startRenderer = panelView.GetComponent<MeshRenderer>();
 
+            var panelViewPosition = view.transform.position;
             var color = _renderer.material.color;
 
-            if (!panelModel.IsJumpBlock
-                || !panelModel.IsAvailable)
+            if (!panelModel.IsJumpBlock)
             {
-                _renderer.material.color = Color.red;
+                color = _startRenderer.material.color;
+            }
+
+            if (!panelModel.IsAvailable)
+            {
                 color = _renderer.material.color;
-                var panelViewPosition = view.transform.position;
-                OnHoverSelected?.Invoke(panelViewPosition, color);
-                return;
+                color = Color.red;
             }
             else
             {
-                _renderer.material.color = Color.green;
                 color = _renderer.material.color;
-                var panelViewPosition = view.transform.position;
-                OnHoverSelected?.Invoke(panelViewPosition, color);
-                return;
+                color = Color.green;
             }
+            OnHoverSelected?.Invoke(panelViewPosition, color);
         }
 
         private void OnPanelExit(PanelView view)
         {
             var panelView = _pairs.First(pair => pair.panelView == view).panelView;
-            _renderer = panelView.GetComponent<MeshRenderer>();
+            _startRenderer = panelView.GetComponent<MeshRenderer>();
 
-            var startColor = _renderer.material.color;
+            var startColor = _startRenderer.material.color;
 
             var panelViewPosition = view.transform.position;
 
