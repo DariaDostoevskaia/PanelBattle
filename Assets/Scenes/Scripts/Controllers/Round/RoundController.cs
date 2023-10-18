8fa7cbe6e22af59a0f237bf2308cdd4e3d1e417f
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-10-18 22:14:15 +0300

    fix: round controller

--- Assets/Scenes/Scripts/Controllers/Round/RoundController.cs
+++ Assets/Scenes/Scripts/Controllers/Round/RoundController.cs
@@ -2,13 +2,18 @@ using System;
 
 namespace LegoBattaleRoyal.Controllers.Round
 {
-    public class RoundController
+    public class RoundController : IDisposable
     {
         public event Action OnRoundChanged;
 
         public void ChangeRound()
         {
             OnRoundChanged?.Invoke();
+        }
+
+        public void Dispose()
+        {
+            OnRoundChanged = null;
         }
     }
 }
