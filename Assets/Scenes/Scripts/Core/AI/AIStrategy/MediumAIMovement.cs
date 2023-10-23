e963cfd1bbbf24ea1fc9f7093b53aef22b2abcd2
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-10-23 20:05:02 +0300

    feat: TryUseToCaptureStrategy

--- Assets/Scenes/Scripts/Core/AI/AIStrategy/MediumAIMovement.cs
+++ Assets/Scenes/Scripts/Core/AI/AIStrategy/MediumAIMovement.cs
@@ -4,10 +4,13 @@ using System;
 
 public class MediumAIMovement : AIMovementStrategy
 {
+    public int BlocksToCapture { get; }
+
     public MediumAIMovement(int blocksToCapture, GridPosition currentPosition, PanelModel[] panelModels, Guid ownerId)
         : base(blocksToCapture, currentPosition, panelModels, ownerId)
     {
         ReturnWhenLosingCombatZone = true;
+        BlocksToCapture = blocksToCapture;
     }
 
     public override PanelModel Dicide()
@@ -15,9 +18,31 @@ public class MediumAIMovement : AIMovementStrategy
         if (TryUsePathfindingStrategy(out var panel))
             return panel;
 
+        if (TryUseToCaptureStrategy(out panel))
+            return panel;
         //TryUseToCaptureStrategy если 5блоков то домой blocksToCapture - через config
 
         panel = UseRandomStrategy();
         return panel;
+    }
+
+    private bool TryUseToCaptureStrategy(out PanelModel panelModel)
+    {
+        panelModel = null;
+        var occupiedBlockCount = BlocksToCapture;
+        foreach (var block in PanelModels)
+        {
+            if (block.IsOccupated(OwnerId))
+            {
+                occupiedBlockCount--;
+            }
+        }
+
+        if (occupiedBlockCount == 0)
+        {
+            CreateNewPathToHome();
+            return true;
+        }
+        return false;
     }
 }
