cc44cd3c27c2c15f04b065180dfbf83742fc51ef
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2024-02-21 00:37:44 +0300

    fix: progress bar

--- Assets/Scenes/Scripts/Presentation/UI/LoadingPanel/LoadingScreenUI.cs
+++ Assets/Scenes/Scripts/Presentation/UI/LoadingPanel/LoadingScreenUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,39 @@ namespace LegoBattaleRoyal.Presentation.UI.LoadingPopup
     public class LoadingScreenUI : MonoBehaviour
     {
         [SerializeField] private Slider _progressBar;
+        private readonly int _total = 100;
+
+        private void Start()
+        {
+            StartLoading();
+        }
+
+        public void StartLoading()
+        {
+            IProgress<int> progress = new Progress<int>((progressValue) =>
+            {
+                float percent = (float)progressValue / _total;
+                SetProgress(percent);
+            });
+
+            // Имитация загрузки данных
+            LoadData(progress);
+        }
+
+        private void LoadData(IProgress<int> progress)
+        {
+            // Логика загрузки данных с обновлением прогресса
+            for (int i = 0; i <= 100; i++)
+            {
+                progress.Report(i); // Сообщить о прогрессе загрузки
+            }
+        }
+
+        private void SetProgress(float percent)
+        {
+            if (_progressBar != null)
+                _progressBar.value = percent;
+        }
 
         public void Show()
         {
