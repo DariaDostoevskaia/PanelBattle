e7e848bd62b9daf2abbbe17f1f9f800481c8da78
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2024-05-10 01:05:10 +0300

    fix: exercise2 - image, create sprite

--- Assets/Scenes/Scripts/EducationAsync/Exercise2Async.cs
+++ Assets/Scenes/Scripts/EducationAsync/Exercise2Async.cs
@@ -3,20 +3,17 @@ using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace LegoBattaleRoyal.EducationAsync
 {
     public class Exercise2Async : MonoBehaviour
     {
         [SerializeField] private string _imageUrl = "https://dummyimage.com/500";
-        [SerializeField] private GameObject _texture;
-
-        private Renderer _renderer;
+        [SerializeField] private Image _texture;
 
         private async void Start()
         {
-            _renderer = _texture.GetComponent<Renderer>();
-
             StartCoroutine(LoadTextureWithCoroutine());
 
             LoadTextureWithUniTask().Forget();
@@ -25,7 +22,8 @@ namespace LegoBattaleRoyal.EducationAsync
 
         private void SetTextureOnObject(Texture2D texture)
         {
-            _renderer.material.mainTexture = texture;
+            _texture.sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), Vector2.zero);
+            _texture.SetNativeSize();
         }
 
         private UnityWebRequest GetTexture()
