7b534ed56dbacc7036f814d3b9f0423ec099aa36
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2024-05-13 02:28:50 +0300

    feat: exercise 9

---
+++ Assets/Scenes/Scripts/EducationAsync/Exercise9Async.cs
@@ -0,0 +1,40 @@
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LegoBattaleRoyal.EducationAsync
+{
+    public class Exercise9Async : MonoBehaviour
+    {
+        private readonly string _text = "The timer went off";
+
+        private async void Start()
+        {
+            await StartTimerUniTask();
+            await StartTimerTask();
+
+            StartCoroutine(StartTimerCoroutine(8));
+        }
+
+        private IEnumerator<WaitForSeconds> StartTimerCoroutine(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            Debug.Log(_text);
+        }
+
+        private async UniTask StartTimerUniTask()
+        {
+            await UniTask.Delay(3000);
+
+            Debug.Log(_text);
+        }
+
+        private async Task StartTimerTask()
+        {
+            await UniTask.Delay(7000);
+
+            Debug.Log(_text);
+        }
+    }
+}
