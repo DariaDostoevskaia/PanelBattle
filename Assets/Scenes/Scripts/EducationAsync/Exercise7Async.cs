ec1045d590295b1ceb2de44f732ab6f57f104ca3
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2024-05-13 22:05:46 +0300

    fix: exercise 7: coroutine, task

--- Assets/Scenes/Scripts/EducationAsync/Exercise7Async.cs
+++ Assets/Scenes/Scripts/EducationAsync/Exercise7Async.cs
@@ -1,29 +1,52 @@
 using Cysharp.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace LegoBattaleRoyal.EducationAsync
 {
     public class Exercise7Async : MonoBehaviour
     {
+        private List<string> _worldsList = new List<string>()
+        {
+            "Bob",
+            "Menson",
+            "Merline",
+            "Anna",
+            "Paul",
+            "Ron"
+        };
+
         private async void Start()
         {
-            await PrintElementAsync();
+            await PrintElementForUniTaskAsync();
+            await PrintElementForTaskAsync();
+
+            StartCoroutine(PrintElementForCoroutine());
         }
 
-        private async UniTask PrintElementAsync()
+        private IEnumerator<WaitForSeconds> PrintElementForCoroutine()
         {
-            var worldsList = new List<string>()
+            foreach (var world in _worldsList)
             {
-                "Bob",
-                "Menson",
-                "Merline",
-                "Anna",
-                "Paul",
-                "Ron"
-            };
+                new WaitForSeconds(2);
+                Debug.Log(world);
+            }
+            yield return new WaitForSeconds(0);
+        }
 
-            foreach (var world in worldsList)
+        private async Task PrintElementForTaskAsync()
+        {
+            foreach (var world in _worldsList)
+            {
+                await Task.Delay(2000);
+                Debug.Log(world);
+            }
+        }
+
+        private async UniTask PrintElementForUniTaskAsync()
+        {
+            foreach (var world in _worldsList)
             {
                 await UniTask.WaitForSeconds(2);
                 Debug.Log(world);
