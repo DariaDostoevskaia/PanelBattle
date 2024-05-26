27c3c573c66a382e447c4e650bfdcb902ee31b6e
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2024-05-26 03:10:45 +0300

    feat: example 9 exercise

--- Assets/Scenes/Scripts/EducationAsync/Exercise9Async.cs
+++ Assets/Scenes/Scripts/EducationAsync/Exercise9Async.cs
@@ -1,6 +1,7 @@
 using Cysharp.Threading.Tasks;
-using System.Collections.Generic;
+using System.Collections;
 using System.Threading.Tasks;
+using System.Timers;
 using UnityEngine;
 
 namespace LegoBattaleRoyal.EducationAsync
@@ -9,33 +10,88 @@ namespace LegoBattaleRoyal.EducationAsync
     {
         private readonly string _text = "The timer went off";
 
+        private float _timerInterval = 5f;
+        private bool _useTimer = true;
+
         private async void Start()
         {
             await StartTimerUniTask();
             await StartTimerTask();
 
-            StartCoroutine(StartTimerCoroutine(8));
+            StartCoroutine(StartTimerCoroutine());
 
             //Timer ++
             //Update ++
+
+            //TimerExample();
         }
 
-        private IEnumerator<WaitForSeconds> StartTimerCoroutine(float seconds)
+        private void TimerExample()
         {
-            yield return new WaitForSeconds(seconds);
+            if (_useTimer)
+            {
+                var timer = new Timer(_timerInterval * 1000);
+
+                timer.Elapsed += OnTimerElapsed;
+                timer.AutoReset = true;
+                timer.Enabled = true;
+            }
+        }
+
+        private static void OnTimerElapsed(object source, System.Timers.ElapsedEventArgs e)
+        {
+            Debug.Log("Timer elapsed!");
+        }
+
+        //private void Update()
+        //{
+        //    if (!_useTimer)
+        //    {
+        //        _timerInterval -= Time.deltaTime;
+
+        //        if (_timerInterval <= 0f)
+        //        {
+        //            OnTimerElapsed(null, null);
+        //            _timerInterval = 5f;
+        //            // —брос таймера
+        //        }
+        //    }
+        //}
+
+        private IEnumerator StartTimerCoroutine()
+        {
+            var seconds = 6;
+            for (int i = 1; i <= seconds; i++)
+            {
+                yield return new WaitForSeconds(1);
+                Debug.Log(i);
+            }
+
             Debug.Log(_text);
         }
 
         private async UniTask StartTimerUniTask()
         {
-            await UniTask.Delay(3000);
+            var seconds = 6;
+
+            for (int i = 1; i <= seconds; i++)
+            {
+                await UniTask.WaitForSeconds(1);
+                Debug.Log(i);
+            }
 
             Debug.Log(_text);
         }
 
         private async Task StartTimerTask()
         {
-            await UniTask.Delay(7000);
+            var seconds = 6;
+
+            for (int i = 1; i <= seconds; i++)
+            {
+                await Task.Delay(1000);
+                Debug.Log(i);
+            }
 
             Debug.Log(_text);
         }
